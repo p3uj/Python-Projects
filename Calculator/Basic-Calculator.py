2a31415c3a253db62d2ec5388f457ef6d99a4f07
Bengie Villesco <bvillesco@gmail.com>
2023-08-18 20:19:18 +0800

    Modify Basic-Calculator.py: Replace the if, elif, else with a match statement and add a userInput function for entering two numbers.

--- Calculator/Basic-Calculator.py
+++ Calculator/Basic-Calculator.py
@@ -1,53 +1,57 @@
-import os   # To clear the screen
-con = 'Y'
-
-while con == 'Y' or con == 'y':
+def userInput():
     print("────────────────────────────────────────────────")
-    print("\t\tBASIC CALCULATOR")
-    print("────────────────────────────────────────────────")
-    print("\t       ADD   MUL   SUB   DIV")
-    print("\t     ─────────────────────────")
-    print("\t     ║  +  ║  x  ║  -  ║  ÷  ║")
-    print("\t     ─────────────────────────")
-    print("\t        1     2     3     4\n")
-    choice = int(input("Enter choice: "))
-
-    # Validation of the input
-    while choice > 4 or choice < 1:
-        choice = int(input("\nWARNING: INVALID CHOICE!\nEnter choice: "))
-
-    os.system("cls")    # Clear the screen
-    print("────────────────────────────────────────────────")
-    if choice == 1:
-        print("\t\tADDITION")
-    elif choice == 2:
-        print("\t\tMULTIPLICATION")
-    elif choice == 3:
-        print("\t\tSUBTRACTION")
-    else:
-        print("\t\tDIVITION")
-    print("────────────────────────────────────────────────")
-
     firstNum = float(input("Enter first number: "))
     secondNum = float(input("Enter second number: "))
+    return firstNum, secondNum
 
-    # Display the inputted numbers and result
-    if choice == 1:
-        print(f"\n{firstNum} + {secondNum} = {firstNum + secondNum}")
-    elif choice == 2:
-        print(f"\n{firstNum} x {secondNum} = {firstNum * secondNum}")
-    elif choice == 3:
-        print(f"\n{firstNum} - {secondNum} = {firstNum - secondNum}")
-    else:
-        print(f"\n{firstNum} ÷ {secondNum} = {firstNum / secondNum}")
+def main():
+    import os   # To clear the screen
+    con = 'Y'
 
-    con = input("\nDo you want to continue this program? (Y/N): ")
+    while con == 'Y' or con == 'y':
+        print("────────────────────────────────────────────────")
+        print("\t\tBASIC CALCULATOR")
+        print("────────────────────────────────────────────────")
+        print("\t       ADD   MUL   SUB   DIV")
+        print("\t     ─────────────────────────")
+        print("\t     ║  +  ║  x  ║  -  ║  ÷  ║")
+        print("\t     ─────────────────────────")
+        print("\t        1     2     3     4\n")
+        choice = int(input("Enter choice: "))
 
-    # Validation of the input
-    while con != 'Y' and con != 'y' and con != 'N' and con != 'n':
-        con = input("\nWARNING: INVALID INPUT!\nDo you want to continue this program? (Y/N): ")
-    os.system("cls")    # Clear the screen
+        # Validation of the input
+        while choice > 4 or choice < 1:
+            choice = int(input("\nWARNING: INVALID CHOICE!\nEnter choice: "))
 
-print("────────────────────────────────────────────────")
-print("\t\tEND OF THE PROGRAM!")
-print("────────────────────────────────────────────────")
+        os.system("cls")    # Clear the screen
+        print("────────────────────────────────────────────────")
+        match choice:
+            case 1 : 
+                print("\t\tADDITION")
+                firstNum, secondNum = userInput()
+                print(f"\n{firstNum} + {secondNum} = {firstNum + secondNum}")
+            case 2 : 
+                print("\t\tMULTIPLICATION")
+                firstNum, secondNum = userInput()
+                print(f"\n{firstNum} x {secondNum} = {firstNum * secondNum}")
+            case 3 : 
+                print("\t\tSUBTRACTION")
+                firstNum, secondNum = userInput()
+                print(f"\n{firstNum} - {secondNum} = {firstNum - secondNum}")
+            case 4 : 
+                print("\t\tDIVITION")
+                firstNum, secondNum = userInput()
+                print(f"\n{firstNum} ÷ {secondNum} = {firstNum / secondNum}")
+
+        con = input("\nDo you want to continue this program? (Y/N): ")
+
+        # Validation of the input
+        while con != 'Y' and con != 'y' and con != 'N' and con != 'n':
+            con = input("\nWARNING: INVALID INPUT!\nDo you want to continue this program? (Y/N): ")
+        os.system("cls")    # Clear the screen
+
+    print("────────────────────────────────────────────────")
+    print("\t\tEND OF THE PROGRAM!")
+    print("────────────────────────────────────────────────")
+
+main()
