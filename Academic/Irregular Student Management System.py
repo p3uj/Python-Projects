eaf0264f1b36633ef95a10747144b304ccc0ccf4
Bengie <bvillesco@gmail.com>
2024-03-29 14:18:20 +0800

    Add codes for create_account function and modified some codes in the main function.

--- Academic/Irregular Student Management System.py
+++ Academic/Irregular Student Management System.py
@@ -1,14 +1,36 @@
 from os import system
 from msvcrt import getch
+accounts_created = {}
 
 def login():
-    print("LOGIN")
+    system("cls")
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print("LOGIN ACCOUNT".center(80))
+    print("────────────────────────────────────────────────────────────────────────────────")
 
 def create_account():
     system("cls")
-    print("CREATE ACCOUNT")
-    print("Press any key to go back to main...", end='', flush=True)
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print("CREATE ACCOUNT".center(80))
+    print("────────────────────────────────────────────────────────────────────────────────")
+    
+    user_name = input("Create username: ")
+    password = input("Create password: ")
+
+    accounts_created[user_name] = password  # Adding the created username and password to the accounts_created(dict).
+
+    system("cls")
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print("CREATE ACCOUNT".center(80))
+    print("────────────────────────────────────────────────────────────────────────────────")
+
+    print("C O N G R A T U L A T I O N S !".center(80))
+    print("Account successfully created!\n".center(80))
+    print(f"Username: {user_name}".center(80))
+    print(f"Password: {password}\n".center(80))
+    print("\t\t\tPress any key to go back to main...", end='', flush=True)
     getch()
+
 
 def choices():
     print("1. Create Another Account")
@@ -16,8 +38,13 @@ def choices():
     print("3. Exit")
 
 def main():
-    print("1. Create Account")
-    print("2. Exit")
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print("Irregular Student Management System".center(80))
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print("(1) Create Account".center(40) + "|" + "(2) Exit".center(39))
+    print("────────────────────────────────────────────────────────────────────────────────")
+    
+    # Initial menu
     while True:
         try:
             choice = int(input("Enter choice: "))
@@ -28,6 +55,8 @@ def main():
             break
         except ValueError:
             print("\nInvalid input! Please input an integer only!")
+
+    # Main menu
     while True:
         try:
             system("cls")
