a4b6aefc102f9455c876a30e85ccc6f2c967a191
Bengie <bvillesco@gmail.com>
2024-03-29 14:51:32 +0800

    Added header_page function and removed some codes in the main, create account, and login function

--- Academic/Irregular Student Management System.py
+++ Academic/Irregular Student Management System.py
@@ -2,18 +2,14 @@ from os import system
 from msvcrt import getch
 accounts_created = {}
 
-def login():
+def login(title):
     system("cls")
-    print("────────────────────────────────────────────────────────────────────────────────")
-    print("LOGIN ACCOUNT".center(80))
-    print("────────────────────────────────────────────────────────────────────────────────")
+    header_page(title)
 
-def create_account():
+def create_account(title):
     system("cls")
-    print("────────────────────────────────────────────────────────────────────────────────")
-    print("CREATE ACCOUNT".center(80))
-    print("────────────────────────────────────────────────────────────────────────────────")
-    
+    header_page(title)
+
     user_name = input("Create username: ")
     password = input("Create password: ")
 
@@ -31,6 +27,18 @@ def create_account():
     print("\t\t\tPress any key to go back to main...", end='', flush=True)
     getch()
 
+def header_page(title_page):
+    print("────────────────────────────────────────────────────────────────────────────────")
+    print(f"{title_page}".center(80))
+    print("────────────────────────────────────────────────────────────────────────────────")
+    match title_page:
+        case "Irregular Student Management System":
+            print("(1) Create Account".center(40) + "|" + "(2) Exit".center(39))
+        case "CREATE ACCOUNT":
+            print("R E Q U I R E M E N T S !".center(80))
+            print("\tUSERNAME - Minimum of 8 characters and maximum of 16 characters only!")
+            print("\tPASSWORD - Minimum of 8 characters!")
+    print("────────────────────────────────────────────────────────────────────────────────")
 
 def choices():
     print("1. Create Another Account")
@@ -38,18 +46,14 @@ def choices():
     print("3. Exit")
 
 def main():
-    print("────────────────────────────────────────────────────────────────────────────────")
-    print("Irregular Student Management System".center(80))
-    print("────────────────────────────────────────────────────────────────────────────────")
-    print("(1) Create Account".center(40) + "|" + "(2) Exit".center(39))
-    print("────────────────────────────────────────────────────────────────────────────────")
+    header_page("Irregular Student Management System")
     
     # Initial menu
     while True:
         try:
             choice = int(input("Enter choice: "))
             if choice == 1:
-                create_account()
+                create_account("CREATE ACCOUNT")
             else:
                 exit("Program end!")
             break
@@ -63,8 +67,8 @@ def main():
             choices()
             choice = int(input("Enter choice: "))
             match choice:
-                case 1: create_account()
-                case 2: login()
+                case 1: create_account("CREATE ACCOUNT")
+                case 2: login("LOGIN ACCOUNT")
                 case 3: exit("Program end!")
                 case _: print("Invalid choice!")
         except ValueError:
